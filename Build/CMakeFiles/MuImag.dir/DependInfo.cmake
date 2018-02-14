db8ce71219b4d7f9e352f3b3633a0d534a031033
Zhiyhep <zhiy0523@163.com>
2018-02-14 22:14:20 +0800

    bad

---
+++ Build/CMakeFiles/MuImag.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cihenp/g4proj/MuImag/MuImag.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/MuImag.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/MuActionInitialization.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/MuActionInitialization.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/MuDetectorConstruction.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/MuDetectorConstruction.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/MuEventAction.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/MuEventAction.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/MuPhysicsList.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/MuPhysicsList.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/MuPrimaryGeneratorAction.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/MuPrimaryGeneratorAction.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/MuRunAction.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/MuRunAction.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/MuSteppingAction.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/MuSteppingAction.cc.o"
+  "/home/cihenp/g4proj/MuImag/src/PrimaryHits.cc" "/home/cihenp/g4proj/MuImag/Build/CMakeFiles/MuImag.dir/src/PrimaryHits.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/cihenp/geant/geant4_10_02_p02-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "/my_cache/ROOT6.10.02/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
